172969a5891e5edd0d97088351702c51adff579d
patelmanasi <142138381+patelmanasi@users.noreply.github.com>
2026-09-09 13:43:50 +0530

    feat: enhance product management with search, sorting, pagination, price and image filters, individual image deletion, bulk delete, CSV export, image management, and product duplication

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -8,19 +8,11 @@ use Inertia\Middleware;
 class HandleInertiaRequests extends Middleware
 {
     /**
-     * The root template that is loaded on the first page visit.
+     * The root template that's loaded on the first page visit.
      *
      * @var string
      */
     protected $rootView = 'app';
-
-    /**
-     * Determine the current asset version.
-     */
-    public function version(Request $request): ?string
-    {
-        return parent::version($request);
-    }
 
     /**
      * Define the props that are shared by default.
@@ -31,8 +23,10 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
-            'auth' => [
-                'user' => $request->user(),
+
+            'flash' => [
+                'success' => fn() => $request->session()->get('success'),
+                'error' => fn() => $request->session()->get('error'),
             ],
         ];
     }
